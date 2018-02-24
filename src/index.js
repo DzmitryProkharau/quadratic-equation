60a43913047abc04e6fcceb32b3e8d3197fcb589
DzmitryProkharau <dimitriyprokharau@gmail.com>
2018-02-24 15:45:00 +0200

    My task № 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var a, b, c;    
+  var arr = [];
+  equation = equation.replace(/ /g, "");
+  
+  a = parseInt(equation);
+  b = parseInt(equation.substring(equation.indexOf('x^2')+3));
+  c = parseInt(equation.substring( equation.lastIndexOf('*x')+2));
+
+  const D = Math.pow(b,2) - 4*a*c; 
+
+  arr[0] = Math.round((-b+Math.sqrt(D) ) / (2*a));
+  arr[1] = Math.round((-b-Math.sqrt(D) ) / (2*a));
+  
+  arr.sort(function (a, b) {
+      return a - b;      
+  });
+  return arr;
 }
